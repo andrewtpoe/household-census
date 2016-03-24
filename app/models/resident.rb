a56f6b0941eb5a4d589e9bfac741d7c8c1f57906
Andrew T. poe <techapoe@gmail.com>
2016-03-24 00:14:23 -0400

    added additional validations to models

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -2,6 +2,8 @@ class Resident < ActiveRecord::Base
   belongs_to :household
   has_many :vehicles
 
-  validates :household_id, presence: true
+  validates :household_id, numericality: true, presence: true
+  validates :age, numericality: true
+  validates_associated :vehicles
 
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,6 +2,7 @@ class Vehicle < ActiveRecord::Base
   belongs_to :household
   belongs_to :resident
 
-  validates :household_id, presence: true
+  validates :household_id, numericality: true, presence: true
+  validates :resident_id, numericality: true, allow_blank: true
 
 end
